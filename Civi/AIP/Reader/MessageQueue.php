a8196db0c6e58facc3fc438c72ea13ef1c359dd5
johannes <margraf@systopia.de>
2025-06-18 16:19:20 +0200

    29436 fix prefetch

--- Civi/AIP/Reader/MessageQueue.php
+++ Civi/AIP/Reader/MessageQueue.php
@@ -143,7 +143,7 @@ class MessageQueue extends Base
             // declare and bind queue
             $this->channel = $this->connection->channel();
             if(isset($this->config['prefetch'])){
-              $this->channel->basic_qos($this->config['prefetch']);
+              $this->channel->basic_qos(0,$this->config['prefetch'], 0);
             }
             if(isset($this->config['x-queue-type'])){
               $queue_arguments = new AMQPTable(['x-queue-type' => $this->config['x-queue-type']]);
